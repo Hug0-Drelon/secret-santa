e7921dce2dd47c2bcbb760ba84eae3a9e4551dd4
Hug0-Drelon <hugo.drelon@gmail.com>
2021-04-03 21:05:03 +0200

    increment participant when added

--- public/assets/js/collection.js
+++ public/assets/js/collection.js
@@ -2,18 +2,26 @@ const collection = {
 
     addButtonElement: undefined,
 
+    participantsCounter: 0,
+
 
     init: function () {
         collection.addButtonElement = document.getElementById('add-button');
-        collection.addButtonElement.addEventListener('click', collection.handleClickButton);
+        collection.addButtonElement.addEventListener('click', collection.handleClickAddButton);
     },
 
-    handleClickButton: function (evt) {
-        console.log('test');
+    handleClickAddButton: function (evt) {
+        collection.participantsCounter++;
         let templateElement = document.getElementById('participant-form-template');
         let participantFormElement = templateElement.content.cloneNode(true);
         let participantContainerElement = document.querySelector('.form-participants');
         participantContainerElement.appendChild(participantFormElement);
+        let nameInputElement = document.getElementById('event_participants___name___name');
+        let emailInputElement = document.getElementById('event_participants___name___email');
+        nameInputElement.setAttribute('name', '<event[participants]['+collection.participantsCounter.toString()+'][name]');
+        nameInputElement.id = ('event_participants___'+collection.participantsCounter.toString()+'___name');
+        emailInputElement.setAttribute('name', '<event[participants]['+collection.participantsCounter.toString()+'][email]');
+        emailInputElement.id = ('event_participants___'+collection.participantsCounter.toString()+'___email');
     }
 }
 
