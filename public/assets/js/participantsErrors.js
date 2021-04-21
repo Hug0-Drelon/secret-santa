2c6b3a439464b66d176e409d3cd67d2d5ff3964f
Hug0-Drelon <hugo.drelon@gmail.com>
2021-04-21 18:31:46 +0200

    handle participants errors in js

--- public/assets/js/participantsErrors.js
+++ public/assets/js/participantsErrors.js
@@ -5,13 +5,31 @@ const participantsErrors = {
     init: function () {
         participantsErrors.errorsListElement = document.querySelector('.participants-errors-list ul');
         if (participantsErrors.errorsListElement !== null) {
-            participantsErrors.errorsListElement = participantsErrors.errorsListElement.childNodes;
-            participantsErrors.displayErrors();
+            const errorsNodeList = participantsErrors.errorsListElement.childNodes;
+            participantsErrors.displayErrors(errorsNodeList);
         }
     },
 
-    displayErrors: function () {
-        // console.log('test');
+    displayErrors: function (errorsNodeList) {
+        const errorsArray = Array.from(errorsNodeList);
+        let sortedErrorsArray = [];
+
+        errorsArray.filter((element, index) => {
+            if (!sortedErrorsArray.includes(element.innerText)) {
+                sortedErrorsArray.push(element.innerText);
+            }
+        });
+
+        while (participantsErrors.errorsListElement.lastElementChild) {
+            participantsErrors.errorsListElement.removeChild(participantsErrors.errorsListElement.lastElementChild);
+        };
+        
+        sortedErrorsArray.forEach(element => {
+            const liElement = document.createElement("LI");
+            liElement.innerText = element;
+            participantsErrors.errorsListElement.appendChild(liElement);
+        });
+        
         document.querySelector('.participants-errors-list').removeAttribute('hidden');
     }
 }
